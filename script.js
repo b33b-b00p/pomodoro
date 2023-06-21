cd4f7ec05033b62f07fb18fd0c69baa3d44b507e
b33b-b00p <103651421+b33b-b00p@users.noreply.github.com>
2023-06-21 15:00:46 -0400

    different color schemes on different timer modes

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let circularProgressBar = document.querySelector("#circularProgressBar"); //ring
+let timerContainer = document.querySelector("#timerContainer");
+
+let circularProgressBar = document.querySelector("#circularProgressBar");
 let progressValue = document.querySelector("#progressValue");
 
 let buttonLeft = document.querySelector("#buttonLeft");
 let buttonRight =  document.querySelector("#buttonRight");
 let buttonLabel = document.querySelector("#buttonLabel");
 
+let buttonContainer = document.querySelector("#buttonContainer");
 let playButton = document.querySelector('#playButton');
 let pauseButton = document.querySelector('#pauseButton');
 let resetButton = document.querySelector('#resetButton');
@@ -20,6 +23,8 @@ let timerInterval;
 let currentTimerType = "work"
 let currentTimerValue = workTime;
 let visualRingProgress = 360 / currentTimerValue;
+let visualRingProgressColor = "#f51b1b";
+let pulseAnimationChoice = "pulseWork";
 
 // **** timer functions ****
 function createStringNumber(number) 
@@ -42,6 +47,7 @@ function startTimer()
     timerInterval = setInterval(decrementTimer, 1000);
     playButton.style.display = "none";
     pauseButton.style.display = "block";
+    circularProgressBar.style.animationName = pulseAnimationChoice;
 }
 
 function pauseTimer()
@@ -49,6 +55,7 @@ function pauseTimer()
     clearInterval(timerInterval);
     playButton.style.display = "block";
     pauseButton.style.display = "none";
+    circularProgressBar.style.animationName = "none";
 }
 
 
@@ -60,7 +67,7 @@ function updateVisualTimer() {
     }
 
     progressValue.textContent = `${createStringNumber(currentTimerValue)}`;
-    circularProgressBar.style.background = `conic-gradient(yellow ${currentTimerValue * visualRingProgress}deg, red 0deg)`;
+    circularProgressBar.style.background = `conic-gradient(#616161 ${currentTimerValue * visualRingProgress}deg, ${visualRingProgressColor} 0deg)`;
 }
 
 function resetTimer() 
@@ -83,9 +90,11 @@ function resetTimer()
     }
     
     visualRingProgress = 360 / currentTimerValue;
-    updateVisualTimer();
     playButton.style.display = "block";
     pauseButton.style.display = "none";
+    circularProgressBar.style.animationName = "none";
+    updateVisualTimer();
+    changeColorScheme();
     //stop audio
 }
 
@@ -135,6 +144,45 @@ function choseTimerMode()
     });
 }
 
+function changeColorScheme()
+{
+    switch(currentTimerType)
+    {
+        case "work":
+            timerContainer.style.boxShadow = "#f51b1b 0px 0px 24px 12px, #f51b1b 0px 4px 6px -1px, #f51b1b 0px 1px 0px inset";
+            buttonLeft.style.backgroundColor = "#f51b1b";
+            buttonRight.style.backgroundColor = "#f51b1b";
+            buttonLabel.style.border = "0.1em solid #f51b1b";
+            buttonContainer.style.backgroundColor = "#f51b1b";
+            buttonContainer.style.border = "4px solid #f51b1b";
+            visualRingProgressColor = "#f51b1b";
+            pulseAnimationChoice = "pulseWork";
+            break;
+        case "shortBreak":
+            timerContainer.style.boxShadow = "#00ffff 0px 0px 24px 12px, #00ffff 0px 4px 6px -1px, #00ffff 0px 1px 0px inset";
+            buttonLeft.style.backgroundColor = "#00ffff";
+            buttonRight.style.backgroundColor = "#00ffff";
+            buttonLabel.style.border = "0.1em solid #00ffff";
+            buttonContainer.style.backgroundColor = "#00ffff";
+            buttonContainer.style.border = "4px solid #00ffff";
+            visualRingProgressColor = "#00ffff";
+            pulseAnimationChoice = "pulseShortBreak";
+            break;
+        case "longBreak":
+            timerContainer.style.boxShadow = "#07eb10 0px 0px 24px 12px, #07eb10 0px 4px 6px -1px, #07eb10 0px 1px 0px inset";
+            buttonLeft.style.backgroundColor = "#07eb10";
+            buttonRight.style.backgroundColor = "#07eb10";
+            buttonLabel.style.border = "0.1em solid #07eb10";
+            buttonContainer.style.backgroundColor = "#07eb10";
+            buttonContainer.style.border = "4px solid #07eb10";
+            visualRingProgressColor = "#07eb10";
+            pulseAnimationChoice = "pulseLongBreak";
+            break;
+        default:
+            console.log("switch case didn't work... T-T");
+    }
+    resetTimer();
+}
 // **** execution ****
 choseTimerMode();
 playButton.addEventListener('click', startTimer);
